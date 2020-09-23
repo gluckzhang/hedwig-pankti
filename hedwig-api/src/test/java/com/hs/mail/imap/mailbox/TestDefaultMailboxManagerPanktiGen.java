d8346ffde15e70ce354a391fa5b1ee63efdd9f98
GluckZhang <zhanglong3030@qq.com>
2020-09-23 14:24:16 +0200

    the Pankti generated test cases which are passed

--- hedwig-api/src/test/java/com/hs/mail/imap/mailbox/TestDefaultMailboxManagerPanktiGen.java
+++
@@ -1,5 +0,0 @@
-package com.hs.mail.imap.mailbox;
-import com.thoughtworks.xstream.XStream;
-public class TestDefaultMailboxManagerPanktiGen {
-    static XStream xStream = new XStream();
-}
--- hedwig-api/src/test/java/com/hs/mail/util/TestEhCacheWrapperPanktiGen.java
+++
@@ -1,5 +0,0 @@
-package com.hs.mail.util;
-import com.thoughtworks.xstream.XStream;
-public class TestEhCacheWrapperPanktiGen {
-    static XStream xStream = new XStream();
-}
